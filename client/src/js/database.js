679107bddc2af73a9a66e26f17e20e3214f7fb2f
Mauricio-tech <mauricio.trevinon91@gmail.com>
2023-05-22 19:42:45 -0600

    Cleaning code

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,56 +1,49 @@
 import { openDB } from "idb";
 
-const initdb = async () =>
-  openDB("jate", 1, {
-    upgrade(db) {
-      if (db.objectStoreNames.contains("jate")) {
-        console.log("jate database already exists");
-        return;
-      }
-      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
-      console.log("jate database created");
-    },
-  });
+// Initialize the database
+const initdb = async () => {
+  try {
+    const db = await openDB("jate", 1);
+    if (db.objectStoreNames.contains("jate")) {
+      console.log("jate database already exists");
+      return;
+    }
+    db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+    console.log("jate database created");
+  } catch (error) {
+    console.error("Error initializing the database:", error);
+  }
+};
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// Add content to the database
 export const putDb = async (content) => {
-  const jDb = await openDB("jate", 1);
-
-  // Create a new transaction and specify the database and data privileges.
-  const tx = jDb.transaction("jate", "readwrite");
-
-  // Open up the desired object store.
-  const store = tx.objectStore("jate");
-
-  // Use the .getAll() method to get all data in the database.
-  const request = store.put({ id: 1, value: content });
-
-  // Get confirmation of the request.
-  const result = await request;
-  console.log("result.value", result);
-  // return result.value;
+  try {
+    const jDb = await openDB("jate", 1);
+    const tx = jDb.transaction("jate", "readwrite");
+    const store = tx.objectStore("jate");
+    const request = store.put({ id: 1, value: content });
+    const result = await request;
+    console.log("Result:", result);
+  } catch (error) {
+    console.error("Error adding content to the database:", error);
+  }
 };
 
-// TODO: Add logic for a method that gets all the content from the database
+// Retrieve content from the database
 export const getDb = async () => {
-  console.log("GET from the database");
-
-  // Create a connection to the database database and version we want to use.
-  const jDb = await openDB("jate", 1);
-
-  // Create a new transaction and specify the database and data privileges.
-  const tx = jDb.transaction("jate", "readonly");
-
-  // Open up the desired object store.
-  const store = tx.objectStore("jate");
-
-  // Use the .getAll() method to get all data in the database.
-  const request = store.getAll();
-
-  // Get confirmation of the request.
-  const result = await request;
-  console.log("result.value", result);
-  return result.value;
+  try {
+    const jDb = await openDB("jate", 1);
+    const tx = jDb.transaction("jate", "readonly");
+    const store = tx.objectStore("jate");
+    const request = store.getAll();
+    const result = await request;
+    console.log("Result:", result);
+    return result.value;
+  } catch (error) {
+    console.error("Error retrieving content from the database:", error);
+    return [];
+  }
 };
 
+// Initialize the database when the module is loaded
 initdb();
